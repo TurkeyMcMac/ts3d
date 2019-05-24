13cf25899d0d6b4a671766f4c6df81342c3c2321
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-05-24 07:35:20 -0400

    Remove stale commented code

--- src/grow.c
+++ src/grow.c
@@ -3,8 +3,6 @@
 
 void *growe(void **bufp, size_t *lenp, size_t *capp, size_t esize)
 {
-	//printf("buf = %p, len = %lu, cap = %lu, esize = %lu\n",
-	//	*bufp, *lenp, *capp, esize);
 	if (*lenp >= *capp) {
 		size_t new_cap = *lenp * 3 / 2 + 1;
 		void *new_buf = realloc(*bufp, new_cap * esize);
@@ -18,7 +16,6 @@ void *growe(void **bufp, size_t *lenp, size_t *capp, size_t esize)
 char *growc(char **bufp, size_t *lenp, size_t *capp, size_t num)
 {
 	size_t old_len = *lenp;
-	//printf("'%*s' + %ld\n", *lenp, *bufp, num);
 	*lenp += num;
 	if (*lenp > *capp) {
 		size_t new_cap = *lenp * 3 / 2 + 1;
