160105311187dfa73f7dddbf9338b1e97447adc2
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-05-25 10:50:40 -0400

    Add string pushing functions

--- src/string.c
+++ src/string.c
@@ -1,6 +1,15 @@
 #include "string.h"
 #include "grow.h"
 #include <stdlib.h>
+#include <string.h>
+
+int string_init(struct string *str, size_t cap)
+{
+	str->len = 0;
+	str->text = malloc(cap);
+	if (!str->text) return -1;
+	return 0;
+}
 
 char *string_grow(struct string *str, size_t *cap, size_t num)
 {
@@ -10,4 +19,26 @@ char *string_grow(struct string *str, size_t *cap, size_t num)
 void string_shrink_to_fit(struct string *str)
 {
 	str->text = realloc(str->text, str->len);
+}
+
+int string_pushn(struct string *str, size_t *cap, const char *push,
+	size_t npush)
+{
+	char *place = string_grow(str, cap, npush);
+	if (!place) return -1;
+	memcpy(place, push, npush);
+	return 0;
+}
+
+int string_pushz(struct string *str, size_t *cap, const char *push)
+{
+	return string_pushn(str, cap, push, strlen(push));
+}
+
+int string_pushc(struct string *str, size_t *cap, int push)
+{
+	char *place = string_grow(str, cap, 1);
+	if (!place) return -1;
+	*place = push;
+	return 0;
 }
--- src/string.h
+++ src/string.h
@@ -8,8 +8,17 @@ struct string {
 	char *text;
 };
 
+int string_init(struct string *str, size_t cap);
+
 char *string_grow(struct string *str, size_t *cap, size_t num);
 
 void string_shrink_to_fit(struct string *str);
+
+int string_pushn(struct string *str, size_t *cap, const char *push,
+	size_t npush);
+
+int string_pushz(struct string *str, size_t *cap, const char *push);
+
+int string_pushc(struct string *str, size_t *cap, int push);
 
 #endif /* STRING_H_ */
