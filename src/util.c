b79ed6af0a37a16d8c8237007b1eaed37d164f8b
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-05-29 19:07:43 -0400

    Add string duplication function

---
+++ src/util.c
@@ -0,0 +1,8 @@
+#include "util.h"
+#include "xalloc.h"
+
+char *str_dup(const char *str)
+{
+	size_t size = strlen(str) + 1;
+	return memcpy(xmalloc(size), str, size);
+}
---
+++ src/util.h
@@ -0,0 +1,6 @@
+#ifndef UTIL_H_
+#define UTIL_H_
+
+char *str_dup(const char *str);
+
+#endif /* UTIL_H_ */
