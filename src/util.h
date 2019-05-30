9b9cb9fb2c23f9af089d4f2e0ae7bd9d988efa31
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-05-30 18:38:47 -0400

    Add portable attribute utility

--- src/util.h
+++ src/util.h
@@ -1,6 +1,14 @@
 #ifndef UTIL_H_
 #define UTIL_H_
 
+#include <stddef.h>
+
+#ifdef __GNUC__
+#	define ATTRIBUTE(...) __attribute__((__VA_ARGS__))
+#else
+#	define ATTRIBUTE(...)
+#endif
+
 char *str_dup(const char *str);
 
 #endif /* UTIL_H_ */
