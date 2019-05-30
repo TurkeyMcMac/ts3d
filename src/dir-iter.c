34f57c039ac9ad1224c9abd023ad92306100f53c
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-05-30 18:15:39 -0400

    Always close an iterated directory

--- src/dir-iter.c
+++ src/dir-iter.c
@@ -6,7 +6,7 @@
 int dir_iter(const char *path, dir_iter_f iter_fn, void *ctx)
 {
 	int errnum;
-	int ret;
+	int ret = -1;
 	DIR *dir = opendir(path);
 	if (!dir) goto error_opendir;
 	struct dirent *ent;
@@ -14,22 +14,15 @@ int dir_iter(const char *path, dir_iter_f iter_fn, void *ctx)
 	while ((ent = readdir(dir))) {
 		// Ignore dot files:
 		if (*ent->d_name == '.') continue;
-		ret = iter_fn(ent, ctx);
-		if (ret != 0) {
-			errnum = errno;
-			closedir(dir);
-			errno = errnum;
-			return abs(ret);
-		}
+		ret = abs(iter_fn(ent, ctx));
+		if (ret != 0) goto end;
 		errno = 0;
 	}
-	if (errno) goto error_readdir;
-	return 0;
-
-error_readdir:
+	ret = -errno;
+end:
 	errnum = errno;
 	closedir(dir);
 	errno = errnum;
 error_opendir:
-	return -1;
+	return ret;
 }
