3df732f61c780a6073ecf92b64dec13026465a04
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-06-12 20:24:53 -0400

    Fix display of transparent pixels

--- src/pixel.c
+++
@@ -1,7 +0,0 @@
-#include "pixel.h"
-
-d3d_pixel pixel_from_char(char ch)
-{
-	d3d_pixel pix = ch - ' ' - 1;
-	return pix <= 64 ? pix : ch;
-}
--- src/pixel.h
+++ src/pixel.h
@@ -5,17 +5,17 @@
 
 #include "d3d.h"
 
-// A completely black pixel.
-#define EMPTY_PIXEL ((d3d_pixel)0)
+// Completely black (for walls) or transparent (for sprites, by default) pixel.
+#define EMPTY_PIXEL ((d3d_pixel)-1)
 
 // The foreground value of the pixel from 0-7.
-#define pixel_fg(pix) ((unsigned)(pix) >> 3)
+#define pixel_fg(pix) ((pix) >> 3 & 7)
 // The background value of the pixel from 0-7.
 #define pixel_bg(pix) ((pix) & 7)
 // Whether or not this pixel should be bold on the screen.
 #define pixel_is_bold(pix) (pixel_bg(pix) != 0)
 
 // Convert a character to a pixel.
-d3d_pixel pixel_from_char(char ch);
+#define pixel_from_char(ch) ((d3d_pixel)((ch) - ' ' - 1))
 
 #endif /* PIXEL_H_ */
