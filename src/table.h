5dd93cfbb4d7d567a136aa332dc7e247ab1e1144
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2019-08-18 09:39:27 -0400

    Remove table freezing requirement

--- src/table.h
+++ src/table.h
@@ -15,25 +15,23 @@ typedef struct {
 // size.
 void table_init(table *tbl, size_t size);
 
-// Add an item to the table. This must be called BEFORE table_freeze. Zero is
-// returned on success, but negative is returned when the key is already in the
-// table.
+// Add an item to the table. Zero is returned on success, but negative is
+// returned when the key is already in the table.
 int table_add(table *tbl, const char *key, void *val);
 
-// Freeze the table, so no more items can be added, but now it can be searched.
+// Freeze the table so no more items can be added. This is not necessary.
 void table_freeze(table *tbl);
 
-// Get an item. This must be called AFTER table_freeze. On success, a mutable
-// pointer to the value is returned. When the key is not present, NULL is
-// returned.
+// Get an item. pointer to the value is returned. When the key is not present,
+// NULL is returned.
 void **table_get(table *tbl, const char *key);
 
-// Iterate through all the entries (AFTER table_freeze is called.) The first
-// argument to the given function is the key, and the second is the mutable
-// pointer to the corresponding value. The order is undefined.
+// Iterate through all the entries. The first argument to the given function is
+// the key, and the second is the mutable pointer to the corresponding value.
+// The order is undefined.
 int table_each(table *tbl, int (*item)(const char *, void **));
 
-// Free all memory allocated for a table, before or after table_freeze
+// Free all memory allocated for a table.
 void table_free(table *tbl);
 
 // Convert a table to an allocated string for debugging. The passed function
