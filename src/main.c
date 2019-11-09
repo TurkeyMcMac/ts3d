c761d617facbba3080b3106b64325500a02c6070
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2019-11-08 19:38:51 -0500

    Add file path configuration options

--- src/main.c
+++ src/main.c
@@ -1,11 +1,22 @@
 #include "do-ts3d-game.h"
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-	const char *play_as = argc > 1 ? argv[1] : NULL;
-	const char *data_dir = "data";
-	const char *state_file = "state.json";
+	char *play_as = NULL, *data_dir = "data", *state_file = "state.json";
+	int opt;
+	while ((opt = getopt(argc, argv, "d:s:")) >= 0) {
+		switch (opt) {
+		case 'd':
+			data_dir = optarg;
+			break;
+		case 's':
+			state_file = optarg;
+			break;
+		}
+	}
+	if (optind < argc) play_as = argv[optind];
 	return do_ts3d_game(play_as, data_dir, state_file) ?
 		EXIT_FAILURE : EXIT_SUCCESS;
 }
