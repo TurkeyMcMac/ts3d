3b6bcdc57f23ce8fcbae4d6dd54cba27d23d43a7
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-04-18 13:53:57 -0400

    Use safer growing algorithm

--- src/grow.c
+++ src/grow.c
@@ -4,7 +4,7 @@
 void *growe(void **bufp, size_t *lenp, size_t *capp, size_t esize)
 {
 	if (*lenp >= *capp) {
-		*capp = *lenp * 3 / 2 + 1;
+		*capp = *lenp + *lenp / 2 + 1;
 		*bufp = xrealloc(*bufp, *capp * esize);
 	}
 	return (char *)*bufp + (*lenp)++ * esize;
@@ -15,7 +15,7 @@ char *growc(char **bufp, size_t *lenp, size_t *capp, size_t num)
 	size_t old_len = *lenp;
 	*lenp += num;
 	if (*lenp > *capp) {
-		*capp = *lenp * 3 / 2 + 1;
+		*capp = *lenp + *lenp / 2 + 1;
 		*bufp = xrealloc(*bufp, *capp);
 	}
 	return *bufp + old_len;
